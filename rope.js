e44f9a83f0ef44927842d39432c4da26bc73cf9b
Mokshi2009 <144035877+Mokshi2009@users.noreply.github.com>
2023-12-11 19:41:43 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,34 @@
+class rope{
+	constructor(body1,body2, offsetX)
+	{
+		var options={
+			bodyA:body1,
+			bodyB:body2
+		}
+
+		this.rope=Constraint.create(options)
+		World.add(world,this.rope)
+
+		this.offsetX = offsetX
+		
+	}
+
+	display()
+	{
+		var pointA=this.rope.bodyA.position;
+		var pointB=this.rope.bodyB.position;
+
+		strokeWeight(2);
+
+		//WRITE THE CORRECT CODE TO RENDER A LINE BETWEEN THE TWO BODIES
+		
+		//line (con.bodyA.x,con.bodyA.y,bodyB.position.x,bodyB.position.y);
+		
+		//line (this.rope.bodyA.position.x,this.rope.bodyA.position.y,this.rope.bodyB.position.x,this.rope.bodyB.position.y);
+		push ()
+		//line (con.pointA.x,con.pointA.y,pointB.x,pointB.y);
+		line (pointA.x,pointA.y,pointB.x+this.offsetX,pointB.y);
+		pop ()
+	}
+
+}
